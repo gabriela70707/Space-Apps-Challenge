de4add72ec66df0c4fe375b35a601341099c93f8
Gabriela Alejandra <gabrielasantos70707@gmail.com>
2025-10-04 23:12:38 -0300

    adding the button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Button from './Components/Button/Button'
 import './App.css'
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        <Button text={"Tá funcionando"}/>
       </p>
     </>
   )
