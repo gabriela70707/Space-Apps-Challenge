ac8017c17240c8e4a361bf28746b4081d0839145
ArianeDev <silvaarianeoliveira2@gmail.com>
2025-10-05 12:45:23 -0300

    Alterei a página inicial e a segunda página, porém não terminei

--- src/Pages/Inicial/Inicial.jsx
+++ src/Pages/Inicial/Inicial.jsx
@@ -1,24 +1,24 @@
 import styles from './Inicial.module.css';
-import img from '../../assets/space-icon.png';
 import { Link } from 'react-router-dom';
 import Button from '../../Components/Button/Button'
+import Earth from '../../Components/Earth';
 
 export function Inicial() {
     return (
-        <>
-            <main className={styles.main}>
+        <main className={styles.main}>
 
             <section className={styles.section}>
                 <h1>Explore the Space</h1>
+                <hr />
                 <p>Did you know that the sun, our giant star, 
-                    sometimes has energy “burps” and “sneezes”? It is called Space Weather! 
+                    sometimes has energy “burps” and “sneezes”? 
+                    It is called Space Weather! 
                 </p>
-               <Link to="/next-page"><Button text={"Continue"}/></Link>
+            <Link to="/next-page"><Button text={"Continue"}/></Link>
             </section>
 
-            <img src={img} alt="Space Icon"/>
+            <Earth />
 
-            </main>
-        </>
+        </main>
     );
 }
--- src/Pages/Next-Page/Next-page.jsx
+++ src/Pages/Next-Page/Next-page.jsx
@@ -3,33 +3,61 @@ import { Link } from 'react-router-dom';
 import star from '../../assets/Star.png';
 import img from '../../assets/space-icon.png';
 import Astro from '../../assets/AstroWithDialog.png';
+import { useEffect, useRef, useState } from 'react';
 
 export function NextPage() {
+    const secondSection = useRef(null);
+    const [countdown, setCountdown] = useState(10);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCountdown(prev => {
+                if (prev <= 1) {
+                    clearInterval(interval);
+                    return 0;
+                }
+                return prev - 1;
+            });
+        }, 1000);
+
+        const timer = setTimeout(() => {
+            secondSection.current?.scrollIntoView({ behavior: 'smooth' });
+        }, 100000);
+
+        return () => {
+            clearInterval(interval);
+            clearTimeout(timer);
+        } 
+    }, []);
+
     return (
-        <>
-            <main className={styles.main}>
+        <main className={styles.main}>
 
             <img src={star} alt="Star" />
-            <section className={styles.section}>
-                <h2>
-                    Get into a unbelievable journey to find out how these “Sneezes” travel through
-                    the space and how they can affect our day here, in the Earth. Let’s meet Coronalda,
-                    a very Spacial Wave!
-                </h2>
+            <section className={styles.firstSection}>
+                <div className="">
+                    <p>
+                        Get into a unbelievable journey to find out how these “Sneezes” travel through
+                        the space and how they can affect our day here, in the Earth. Let’s meet Coronalda,
+                        a very Spacial Wave!
+                    </p>
+                    <p>Next part in: {countdown}s</p>
+                </div>
 
                 {/* tem que colocar a setinha pra baixo aqui */}
             </section>
 
-            <div><img src={img} alt="Space Icon"/></div>
+            <div className={styles.earthImg}>
+                <img src={img} alt="Space Icon"/>
+            </div>
 
-            <section>
+            <section ref={secondSection}>
                 <div><img src={Astro} alt="Astro"/></div>
                 <h2>What's your name?</h2>
                 <input type="text" placeholder="Type your name here..."/>
                 <button><Link to="/First-Part">Let's Start</Link></button>
             </section>
 
-            </main>
-        </>
+        </main>
     );
 }
