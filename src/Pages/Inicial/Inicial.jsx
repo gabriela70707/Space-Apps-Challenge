7bcad90f3d483c45833a61041f145e4f25c9f38e
Gabriela Alejandra <gabrielasantos70707@gmail.com>
2025-10-05 07:30:29 -0300

    adding Button

--- src/Pages/Inicial/Inicial.jsx
+++ src/Pages/Inicial/Inicial.jsx
@@ -1,6 +1,7 @@
 import styles from './Inicial.module.css';
 import img from '../../assets/space-icon.png';
 import { Link } from 'react-router-dom';
+import Button from '../../Components/Button/Button'
 
 export function Inicial() {
     return (
@@ -12,7 +13,7 @@ export function Inicial() {
                 <p>Did you know that the sun, our giant star, 
                     sometimes has energy “burps” and “sneezes”? It is called Space Weather! 
                 </p>
-                <button><Link to="/next-page">CONTINUE</Link></button>
+               <Link to="/next-page"><Button text={"Continue"}/></Link>
             </section>
 
             <img src={img} alt="Space Icon"/>
